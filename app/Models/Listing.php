2eb90d17077ff8237a1d5469f639ef783e733be5
MylesMburu <ngicumyles@gmail.com>
2024-03-23 15:16:01 +0300

    updates

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,23 +1,11 @@
 <?php
- 
+
 namespace App\Models;
 
-class Listing {
-    public static function all(){
-        return [
-            ['id' => 1, 'title' => 'First listing', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'],
-            ['id' => 2, 'title' => 'Second listing', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'],
-            ['id' => 3, 'title' => 'Third listing', 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        Listing::create([
+            'title' => 'Web Developer',
+            'tags' => 'php, laravel, javascript',
+            'email' => 'example@gmail.com',
+            'company' => 'Acme Inc.',
+            'location' => 'New York, NY',
+            'website' => 'http://www.acme.com',
+            'description' => 'We are looking for a web developer to join our team.'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
